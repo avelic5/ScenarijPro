cd19e7c927d31f83ebab1a0fa063234ed37c8909
avelic5 <avelic5@etf.unsa.ba>
2025-11-13 17:30:43 +0100

    Start spirala2

---
+++ js/EditorTeksta.js
@@ -0,0 +1,33 @@
+let EditorTeksta = function (divRef) {
+    //privatni atributi modula
+
+    //implementacija metoda
+    let dajBrojRijeci = function () {
+    };
+
+    let dajUloge = function () {
+    }
+    let pogresnaUloga= function() {
+    }
+    let brojLinijaTeksta = function (uloga){
+    }
+    let scenarijUloge = function (uloga){
+    }
+    let grupisiUloge = function (){
+
+    }
+    let formatirajTekst = function (komanda){
+
+    }
+
+    // … na isti način i za ostale metode
+    return {
+        dajBrojRijeci,
+        dajUloge,
+        pogresnaUloga,
+        brojLinijaTeksta,
+        scenarijUloge,
+        grupisiUloge,
+        formatirajTekst
+    }
+};

e84f8682aedb849c13c6e34e8ccd69045f570197
avelic5 <avelic5@etf.unsa.ba>
2026-01-11 16:14:37 +0100

    Izmjene za testove - dodavanje tekstualne datoteke za izmjene i fixanje porta

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs/promises");
 const app = express();
-const PORT = 8080;
+const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
